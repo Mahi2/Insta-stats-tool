cf7358dcec0ccb5e88ee1c94275514e0b9d017fe
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 00:48:53 +0100

    [ADD] Adding extra_setting file

--- views/admin/extra_setting.php
+++ views/admin/extra_setting.php
@@ -4,3 +4,15 @@
     <div class="card-body">
         <h4><?= $language->admin_extra_settings->header2 ?></h4>
         <p class="text-muted"><?= $language->admin_extra_settings->subheader2 ?></p>
+
+
+        <div class="mt-5">
+            <?php if(!$plugins_result->num_rows): ?>
+
+                <?= $language->admin_extra_settings->display->no_plugins ?>
+
+            <?php else: ?>
+                <?php while($plugin = $plugins_result->fetch_object()): ?>
+
+                    <div class="media mb-3">
+                        <div class="mr-3 plugin" style="background: <?= $plugin->color ?>"><?= substr($plugin->name, 0, 1) ?></div>
