f2b2958b1e8feed95f8c47731c8c33ef47dc783e
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 00:48:23 +0100

    [ADD] Adding extra_setting file

--- views/admin/extra_setting.php
+++ views/admin/extra_setting.php
@@ -1,2 +1,6 @@
 <?php defined('ALTUMCODE') || die() ?>
 
+<div class="card card-shadow">
+    <div class="card-body">
+        <h4><?= $language->admin_extra_settings->header2 ?></h4>
+        <p class="text-muted"><?= $language->admin_extra_settings->subheader2 ?></p>
