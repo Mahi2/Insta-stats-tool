d9ea193175c7086ba0136897b6aeb40bed1b0fcc
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 00:58:00 +0100

    [UPDATE] curl version for alert message

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,1 +1,7 @@
 <?php defined('ALTUMCODE') || die() ?>
+
+<?php if(!function_exists('curl_version')): ?>
+    <div class="alert alert-danger" role="alert">
+        <i class="fa fa-minus"></i> Your web server does not have cURL installed and enabled. Please contact your webhost provider or install cURL.
+    </div>
+<?php endif ?>
