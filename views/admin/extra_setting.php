d6d4edadf322aa24105f6660413f3b73deb59c35
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 00:52:02 +0100

    [UPDATE] language admin extra settings

--- views/admin/extra_setting.php
+++ views/admin/extra_setting.php
@@ -40,3 +40,92 @@
         </div>
     </div>
 </div>
+<div class="card card-shadow mt-3">
+    <div class="card-body">
+
+        <h5><?= $language->admin_extra_settings->display->demo_reports ?></h5>
+        <p class="text-muted"><?= $language->admin_extra_settings->display->demo_reports_help ?></p>
+
+        <table class="table table-hover">
+            <thead class="thead-black thead-inverse">
+            <tr>
+                <th><?= $language->admin_extra_settings->table->username ?></th>
+                <th><?= $language->admin_extra_settings->table->is_featured ?></th>
+                <th><?= $language->admin_extra_settings->table->source ?></th>
+                <th><?= $language->admin_extra_settings->table->actions ?></th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <?php while($row = $demo_users_result->fetch_object()): ?>
+                <?php if(!in_array($row->source, $sources)) continue ?>
+
+                <tr>
+                    <td><a href="<?= url('report/' . $row->username . '/' . $row->source) ?>" target="_blank"><?= $row->username ?></td>
+                    <td><?= $row->is_featured ? '<span class="badge badge-pill badge-success"><i class="fa fa-check-circle fa-sm"></i> ' . $language->global->yes . '</span>' : '<span class="badge badge-pill badge-info"><i class="fa fa-times-circle fa-sm"></i> ' . $language->global->no . '</span>' ?></td>
+                    <td><i class="<?= $language->{$row->source}->global->icon ?> text-<?= $row->source ?>"></i> <?= $language->{$row->source}->global->name ?></td>
+                    <td>
+                        <div class="dropdown">
+                            <a href="#" data-toggle="dropdown" class="text-secondary dropdown-toggle dropdown-toggle-simple">
+                                <i class="fas fa-ellipsis-v"></i>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" data-confirm="<?= $language->global->info_message->confirm_delete ?>" href="admin/extra-settings/demo_delete/<?= $row->id . '/' . Security::csrf_get_session_token('url_token') . '/' . $row->source  ?>"><i class="fa fa-times"></i>  <?= $language->global->delete ?></a>
+                                </div>
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+
+            <?php endwhile ?>
+
+            <tr>
+                <td colspan="5">
+                    <form class="form-inline" action="" method="post" role="form">
+                        <input type="hidden" name="form_token" value="<?= Security::csrf_get_session_token('form_token') ?>" />
+                        <input type="hidden" name="type" value="demo_reports" />
+
+                        <div class="mr-4">
+                            <i class="fa fa-plus fa-1x"></i>
+                        </div>
+
+                        <div class="form-group mr-4">
+                            <input type="text" name="username" class="form-control" placeholder="<?= $language->admin_extra_settings->input->username ?>" value="" required="required" />
+                        </div>
+
+                        <div class="form-group mr-4">
+                            <select class="custom-select" name="source">
+                                <option value="instagram">Instagram</option>
+
+                                <?php
+
+                                foreach($plugins->plugins as $plugin_identifier => $value) {
+                                    if($plugins->exists_and_active($plugin_identifier)) {
+                                        echo '<option value="' . $plugin_identifier . '">' . ucfirst($plugin_identifier) . '</option>';
+                                    }
+                                }
+
+                                ?>
+
+                            </select>
+                        </div>
+
+                        <div class="form-group mr-4">
+                            <select class="custom-select" name="is_featured">
+                                <option value="1"><?= sprintf($language->admin_extra_settings->input->is_featured, $language->global->yes) ?></option>
+                                <option value="0"><?= sprintf($language->admin_extra_settings->input->is_featured, $language->global->no) ?></option>
+                            </select>
+                        </div>
+
+                        <div class="text-center">
+                            <button type="submit" name="submit" class="btn btn-primary"><?= $language->global->submit_button ?></button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+
+    </div>
+</div>
