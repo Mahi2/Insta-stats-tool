69f2737bb6589cffd40320429cc0cfe9140d0cec
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 00:58:51 +0100

    [UPDATE] version compare

--- views/admin/index.php
+++ views/admin/index.php
@@ -11,3 +11,9 @@
         <i class="fa fa-minus"></i> Your web server disabled the <strong>iconv()</strong> php function. Please contact your webhost provider or install php with iconv().
     </div>
 <?php endif ?>
+
+<?php if(version_compare(PHP_VERSION, '7.0.0', '<')): ?>
+    <div class="alert alert-danger" role="alert">
+        <i class="fa fa-minus"></i> You are on PHP Version <strong><?= PHP_VERSION ?></strong> and the script requires at least <strong>PHP 7 or above</strong>.
+    </div>
+<?php endif ?>
