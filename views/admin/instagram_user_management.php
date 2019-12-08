1ca07f7b809a05525212bfd5908f0ff753c8fc3c
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 12:07:15 +0100

    [UPDATE] instagram users import

--- views/admin/instagram_user_management.php
+++ views/admin/instagram_user_management.php
@@ -1,1 +1,5 @@
 <?php defined('ALTUMCODE') || die() ?>
+
+<div class="d-flex justify-content-between mb-3">
+    <a href="admin/instagram-users-import" class="btn btn-primary"><i class="fa fa-cloud-upload-alt"></i> <?= $language->instagram->admin_users_management->display->instagram_users_import ?></a>
+</div>
