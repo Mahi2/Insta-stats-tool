d19fdb8b33ca6018130927aa6a1622b41068f42e
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 13:07:10 +0100

    [UPDATE] shared includes

--- views/admin/wrapper.php
+++ views/admin/wrapper.php
@@ -1,1 +1,22 @@
 <?php defined('ALTUMCODE') || die() ?>
+
+<!DOCTYPE html>
+<html class="<?= $language->direction ?>" dir="<?= $language->direction ?>">
+    <?php require VIEWS_ROUTE . $route . 'shared_includes/head.php' ?>
+
+    <body>
+    <?php require VIEWS_ROUTE . $route . 'shared_includes/menu.php' ?>
+
+
+    <div class="container animated fadeIn">
+        <?php display_notifications() ?>
+
+        <?php require VIEWS_ROUTE . $route . $controller . '.php' ?>
+
+        <?php require VIEWS_ROUTE . $route . 'shared_includes/footer.php' ?>
+    </div>
+
+    <?php perform_event('footer') ?>
+
+    </body>
+</html>
