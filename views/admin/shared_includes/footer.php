ce85498a0bed074bb38e1ea55867c79dfcf4d618
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 13:11:48 +0100

    [UPDATE] footer section

---
+++ views/admin/shared_includes/footer.php
@@ -0,0 +1,8 @@
+<?php defined('ALTUMCODE') || die() ?>
+<hr />
+
+<div class="mb-5">
+    <span class="text-muted"><?= 'Copyright &copy; ' . date('Y') . ' ' . $settings->title . '. All rights reserved. Product by <a href="https://altumcode.io/">AltumCode</a>' ?></span>
+</div>
+
+</div>
