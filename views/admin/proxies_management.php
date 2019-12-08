7d98298520035c1e55f08e4221317227f38a5829
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 12:40:05 +0100

    [UPDATE] testing proxies properly

--- views/admin/proxies_management.php
+++ views/admin/proxies_management.php
@@ -1,1 +1,5 @@
 <?php defined('ALTUMCODE') || die() ?>
+
+<div class="alert alert-info ">
+    <strong>Info</strong> Please test your proxies properly and make sure they work most of the time, do NOT use the proxy feature if you don't know what you're doing!
+</div>
