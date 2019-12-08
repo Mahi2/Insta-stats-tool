e012918ba25401f8276b4f24195bcd2522dc31b7
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 01:07:34 +0100

    [ADD] Adding instagram users import file

--- views/admin/instagram_users_import.php
+++ views/admin/instagram_users_import.php
@@ -1,1 +1,11 @@
 <?php defined('ALTUMCODE') || die() ?>
+
+<div class="card card-shadow mb-3">
+    <div class="card-body">
+
+
+        <h4 class="d-flex justify-content-between">
+            <span><i class="fa fa-cloud-upload-alt"></i> <?= $language->admin_instagram_users_import->header ?></span>
+            <small><?= User::generate_go_back_button('admin/proxies-management') ?></small>
+        </h4>
+        <p class="text-muted"><?= $language->admin_instagram_users_import->header_help ?></p>
