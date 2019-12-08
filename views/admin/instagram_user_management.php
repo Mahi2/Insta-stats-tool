0ad890fa154050de7ea299a048fe3374b798c212
Mahi2 <mahi2hm@outlook.fr>
2019-12-08 12:07:52 +0100

    [UPDATE] management username

--- views/admin/instagram_user_management.php
+++ views/admin/instagram_user_management.php
@@ -3,3 +3,21 @@
 <div class="d-flex justify-content-between mb-3">
     <a href="admin/instagram-users-import" class="btn btn-primary"><i class="fa fa-cloud-upload-alt"></i> <?= $language->instagram->admin_users_management->display->instagram_users_import ?></a>
 </div>
+
+<div class="card card-shadow">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table id="results" class="table">
+                <thead class="thead-black">
+                    <tr>
+                        <th><?= $language->instagram->admin_users_management->table->username ?></th>
+                        <th><?= $language->instagram->admin_users_management->table->full_name ?></th>
+                        <th><?= $language->instagram->admin_users_management->table->last_check_date ?></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    </div>
+</div>
