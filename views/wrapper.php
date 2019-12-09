19c0aa337a54937d70a25f50cd7fd904698550e1
Mahi2 <mahi2hm@outlook.fr>
2019-12-09 16:01:22 +0100

    [ADD] Wrapper of content

--- views/wrapper.php
+++ views/wrapper.php
@@ -1,2 +1,27 @@
 <?php defined('ALTUMCODE') || die() ?>
 
+<!DOCTYPE html>
+<html lang="<?= $language->language_code ?>" class="<?= $language->direction ?>" dir="<?= $language->direction ?>">
+    <?php require VIEWS_ROUTE . $route . 'shared_includes/head.php' ?>
+
+    <body <?= $controller == 'index' ? 'class="index-body"' : null ?>>
+        <?php require VIEWS_ROUTE . $route . 'shared_includes/menu.php' ?>
+
+        <?php if($controller_has_container): ?>
+        <main class="container">
+            <?php display_notifications() ?>
+        <?php endif ?>
+
+
+        <?php require VIEWS_ROUTE . $route . $controller . '.php' ?>
+
+
+        <?php if($controller_has_container): ?>
+        </main>
+        <?php endif ?>
+
+        <?php require VIEWS_ROUTE . $route . 'shared_includes/footer.php' ?>
+
+        <?php perform_event('footer') ?>
+    </body>
+</html>
