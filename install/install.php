c167a02e0bc4470044cf52d3dd3c5aac50ec3365
Mahi2 <mahi2hm@outlook.fr>
2019-12-09 16:43:47 +0100

    [UPDATE] Check if connected on api

---
+++ install/install.php
@@ -0,0 +1,18 @@
+<?php
+define('ROOT', realpath(__DIR__ . '/..') . '/');
+require_once ROOT . 'vendor/autoload.php';
+require_once ROOT . 'core/includes/product.php';
+
+$altumcode_api = 'https://api.altumcode.io/validate';
+
+/* Make sure all the required fields are present */
+$required_fields = ['license', 'database_host', 'database_name', 'database_username', 'database_password', 'url'];
+
+foreach($required_fields as $field) {
+    if(!isset($_POST[$field])) {
+        die(json_encode([
+            'status' => 'error',
+            'message' => 'One of the required fields are missing.'
+        ]));
+    }
+}
