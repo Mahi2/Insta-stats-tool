9820d54accea2bc0660255a655db0b979156d329
Mahi2 <mahi2hm@outlook.fr>
2019-12-09 15:56:30 +0100

    [ADD] resending activation

--- views/resend_activation.php
+++ views/resend_activation.php
@@ -1,2 +1,29 @@
 <?php defined('ALTUMCODE') || die() ?>
 
+<div class="d-flex justify-content-center">
+    <div class="card card-shadow animated fadeIn col-xs-12 col-sm-10 col-md-6 col-lg-4">
+        <div class="card-body">
+
+            <h4 class="card-title d-flex justify-content-between">
+                <?= $language->resend_activation->header ?>
+
+                <small><?= User::generate_go_back_button('login') ?></small>
+            </h4>
+
+            <form action="" method="post" role="form">
+                <div class="form-group mt-5">
+                    <input type="text" name="email" class="form-control form-control-border" value="<?= $email ?>" placeholder="<?= $language->resend_activation->input->email ?>" aria-label="<?= $language->resend_activation->input->email ?>" required="required" />
+                </div>
+
+                <div class="form-group mt-5">
+                      <?php $captcha->display() ?>
+                </div>
+
+                <div class="form-group mt-5">
+                    <button type="submit" name="submit" class="btn btn-default btn-block my-1"><?= $language->global->submit_button ?></button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
