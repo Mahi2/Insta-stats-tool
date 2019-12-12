7cedad89787a795d009a9a9f40305bd2d4c8adbe
Mahi2 <mahi2hm@outlook.fr>
2019-12-12 15:11:51 +0100

    [UPDATE] Clean some posted variables

--- controllers/account_settings.php
+++ controllers/account_settings.php
@@ -4,3 +4,10 @@ User::check_permission(0);
 
 $method 	= (isset($parameters[0])) ? $parameters[0] : false;
 $url_token 	= (isset($parameters[1])) ? $parameters[1] : false;
+
+if(!empty($_POST)) {
+
+    /* Clean some posted variables */
+    $_POST['email']		    = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $_POST['username']		= generate_slug(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+    $_POST['name']		= filter_var($_POST['name'], FILTER_SANITIZE_STRING);
