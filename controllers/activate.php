8a0bb7aa273557f732a0488cbfd3954a4b4ea9a9
Mahi2 <mahi2hm@outlook.fr>
2019-12-12 15:22:52 +0100

    [UPDATE] Get email of user so we can further compare

--- controllers/activate.php
+++ controllers/activate.php
@@ -5,3 +5,8 @@ $md5_email = (isset($parameters[0])) ? $parameters[0] : false;
 $email_activation_code = (isset($parameters[1])) ? $parameters[1] : false;
 
 if(!$md5_email || !$email_activation_code) redirect();
+
+/* Get email of user so we can further compare */
+$this_account = Database::get(['user_id', 'email'], 'users', ['email_activation_code' => $email_activation_code]);
+
+if(!$this_account) redirect();
