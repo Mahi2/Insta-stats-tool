3821162b9af70cac422986a48f990a5ecda241fe
Mahi2 <mahi2hm@outlook.fr>
2019-12-12 15:11:09 +0100

    [ADD] Method and url token

--- controllers/account_settings.php
+++ controllers/account_settings.php
@@ -1,3 +1,6 @@
 <?php
 defined('ALTUMCODE') || die();
 User::check_permission(0);
+
+$method 	= (isset($parameters[0])) ? $parameters[0] : false;
+$url_token 	= (isset($parameters[1])) ? $parameters[1] : false;
