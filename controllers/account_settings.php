d4d5ffb9ba4d1601b8b56c919c483a9680b4bbff
Mahi2 <mahi2hm@outlook.fr>
2019-12-12 15:12:36 +0100

    [UPDATE] Checking for any errors

--- controllers/account_settings.php
+++ controllers/account_settings.php
@@ -11,3 +11,40 @@ if(!empty($_POST)) {
     $_POST['email']		    = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $_POST['username']		= generate_slug(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
     $_POST['name']		= filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+
+    /* Check for any errors */
+    if(!Security::csrf_check_session_token('form_token', $_POST['form_token'])) {
+        $_SESSION['error'][] = $language->global->error_message->invalid_token;
+    }
+    if(strlen($_POST['username']) < 3 || strlen($_POST['username']) > 32) {
+        $_SESSION['error'][] = $language->account_settings->error_message->username_length;
+    }
+    if(Database::exists('user_id', 'users', ['username' => $_POST['username']]) && $_POST['username'] !== $account->username) {
+        $_SESSION['error'][] = sprintf($language->account_settings->error_message->user_exists, $_POST['username']);
+    }
+    if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false) {
+        $_SESSION['error'][] = $language->register->error_message->invalid_email;
+    }
+    if(Database::exists('user_id', 'users', ['email' => $_POST['email']]) && $_POST['email'] !== $account->email) {
+        $_SESSION['error'][] = $language->register->error_message->email_exists;
+    }
+
+    if(strlen($_POST['name']) < 3 || strlen($_POST['name'] > 32)) {
+        $_SESSION['error'][] = $language->register->error_message->name_length;
+    }
+
+    if(!empty($_POST['old_password']) && !empty($_POST['new_password'])) {
+        if(!password_verify($_POST['old_password'], $account->password)) {
+            $_SESSION['error'][] = $language->account_settings->error_message->invalid_current_password;
+        }
+        if(strlen(trim($_POST['new_password'])) < 6) {
+            $_SESSION['error'][] = $language->account_settings->error_message->short_password;
+        }
+        if($_POST['new_password'] !== $_POST['repeat_password']) {
+            $_SESSION['error'][] = $language->account_settings->error_message->passwords_not_matching;
+        }
+    }
+
+
+
+    if(empty($_SESSION['error'])) {
