ec3c4db91ac808565d98553bf2178a259e9a9750
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 14:49:09 +0100

    [UPDATE] Unlocked reports validity

--- controllers/cron.php
+++ controllers/cron.php
@@ -3,3 +3,10 @@ defined('MAHIDCODE') || die();
 
 /* Initiation */
 set_time_limit(0);
+
+/* *****************/
+/* Unlocked reports validity */
+/* *****************/
+
+/* Make sure to clean unlocked reports which are expired */
+$database->query("DELETE FROM `unlocked_reports` WHERE `expiration_date` < NOW() AND `expiration_date` <> 0");
