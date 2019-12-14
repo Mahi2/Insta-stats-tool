4b15dc81c56be02945406aae41fdc80af56db0de
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 14:49:50 +0100

    [UPDATE] Email reporting

--- controllers/cron.php
+++ controllers/cron.php
@@ -10,3 +10,28 @@ set_time_limit(0);
 
 /* Make sure to clean unlocked reports which are expired */
 $database->query("DELETE FROM `unlocked_reports` WHERE `expiration_date` < NOW() AND `expiration_date` <> 0");
+
+/* *****************/
+/* Email reporting */
+/* *****************/
+
+if($settings->email_reports) {
+
+    switch ($settings->email_reports_frequency) {
+        case 'DAILY':
+            $timestampdiff = 'DAY';
+            $timestampdiff_compare = '0';
+            break;
+
+        case 'WEEKLY':
+            $timestampdiff = 'DAY';
+            $timestampdiff_compare = '6';
+
+            break;
+
+        case 'MONTHLY':
+            $timestampdiff = 'MONTH';
+            $timestampdiff_compare = '0';
+
+            break;
+    }
