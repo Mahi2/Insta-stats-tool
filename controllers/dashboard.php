8e1bf830c27b5dda30cbedb348784004d6966b96
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 14:56:42 +0100

    [UPDATE] reporting counts

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,3 +1,6 @@
 <?php
 defined('MAHIDCODE') || die();
 User::check_permission(0);
+
+$reports_count = $database->query("SELECT  COUNT(*) AS `total` FROM `unlocked_reports` LEFT JOIN `instagram_users` ON `unlocked_reports`.`source_user_id` = `instagram_users`.`id` WHERE `user_id` = {$account_user_id}")->fetch_object()->total;
+$favorites_count = $database->query("SELECT COUNT(*) AS `total` FROM `favorites` WHERE `user_id` = {$account_user_id}")->fetch_object()->total;
