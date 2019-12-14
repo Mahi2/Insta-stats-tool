323bd8b2ccb48b2496c5044d5e73b83c7ab412bf
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 15:03:39 +0100

    [UPDATE] Response

--- controllers/directory_ajax.php
+++ controllers/directory_ajax.php
@@ -8,3 +8,9 @@ $error = [];
 if(!isset($_POST['global_form_token']) || isset($_POST['global_form_token']) && !Security::csrf_check_session_token('global_form_token', $_POST['global_form_token'])) {
     $error[] = $language->global->error_message->invalid_token;
 }
+
+/* Response */
+if(!empty($error)) {
+    Response::json($error, 'error');
+    die();
+}
