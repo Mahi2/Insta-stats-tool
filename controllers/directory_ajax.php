f19c21b5cad67041089877068f38054cac6de355
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 15:04:43 +0100

    [UPDATE] Parse the filters

--- controllers/directory_ajax.php
+++ controllers/directory_ajax.php
@@ -19,3 +19,16 @@ if(!empty($error)) {
 $start = (int) filter_var($_POST['start'], FILTER_SANITIZE_NUMBER_INT);
 $real_limit = (int) filter_var($_POST['limit'], FILTER_SANITIZE_NUMBER_INT);
 $limit = $real_limit + 1;
+
+/* Parse the filters */
+$where = '`is_private` = 0 ';
+$order_by_column = '`id`';
+$order_by_criteria = 'ASC';
+
+$bio = false;
+$followers_from = $followers_to = false;
+$engagement_from = $engagement_to = false;
+$order_by_filter = $order_by_type =  false;
+
+
+if(isset($_POST['filters'])) {
