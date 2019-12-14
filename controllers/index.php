47b4c5fad6ea9a81dd4e3dc16351c7c041e4b0e0
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 15:13:18 +0100

    [UPDATE] Include the aos library

--- controllers/index.php
+++ controllers/index.php
@@ -2,3 +2,25 @@
 defined('MAHIDCODE') || die();
 
 $controller_has_container = false;
+
+/* Include the aos library */
+add_event('head', function() {
+    echo '<link href="assets/css/aos.min.css" rel="stylesheet" media="screen">';
+});
+
+
+add_event('footer', function() {
+    echo '<script src="assets/js/aos.min.js"></script>';
+
+    echo <<<ALTUM
+<script>
+    $(document).ready(() => {
+        AOS.init({
+            delay: 50,
+            duration: 600
+        });
+    });
+</script>
+ALTUM;
+
+});
