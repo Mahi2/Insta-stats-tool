983ed83e83d7a881681b609bcc19ed85f7889766
Mahi2 <mahi2hm@outlook.fr>
2019-12-14 15:06:53 +0100

    [UPDATE] Build html for active filters

--- controllers/directory_ajax.php
+++ controllers/directory_ajax.php
@@ -102,3 +102,49 @@ if(isset($_POST['filters'])) {
 
 $reports_result = $database->query("SELECT * FROM `instagram_users` WHERE {$where} ORDER BY {$order_by_column} {$order_by_criteria} LIMIT {$start}, {$limit}");
 $total_results = $reports_result->num_rows;
+
+/* Build html for active filters */
+$active_filters_html = '';
+
+if($order_by_filter) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="order_by_filter_remove"><i class="fas fa-neuter"></i> ' . $language->directory->display->order_by_filter . ': ' . $order_by_filter . ' <i class="fas fa-times"></i></a>
+    ';
+}
+
+if($order_by_type) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="order_by_type_remove"><i class="fas fa-sort"></i> ' . $language->directory->display->order_by_type . ': ' . $order_by_type . ' <i class="fas fa-times"></i></a>
+    ';
+}
+
+if($bio) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="bio_filter_remove"><i class="fas fa-pencil-alt"></i> ' . $language->directory->display->bio . ': ' . $bio . ' <i class="fas fa-times"></i></a>
+    ';
+}
+
+if($followers_from) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="followers_from_filter_remove"><i class="fas fa-users"></i> ' . $language->directory->display->followers_from . ': ' . $followers_from . ' <i class="fas fa-times"></i></a>
+    ';
+}
+
+if($followers_to) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="followers_to_filter_remove"><i class="fas fa-users"></i> ' . $language->directory->display->followers_to . ': ' . $followers_to . ' <i class="fas fa-times"></i></a>
+    ';
+}
+
+if($engagement_from) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="engagement_from_filter_remove"><i class="fas fa-comments"></i> ' . $language->directory->display->engagement_from . ': ' . $engagement_from . ' <i class="fas fa-times"></i></a>
+    ';
+}
+
+if($engagement_to) {
+    $active_filters_html .= '
+        <a href="#" class="badge badge-light badge-pill" id="engagement_to_filter_remove"><i class="fas fa-comments"></i> ' . $language->directory->display->engagement_to . ': ' . $engagement_to . ' <i class="fas fa-times"></i></a>
+    ';
+}
+$order_by_filter = in_array($order_by_filter, ['id', 'username', 'followers', 'following', 'uploads', 'average_engagement_rate']) ? $order_by_filter : 'ASC';
