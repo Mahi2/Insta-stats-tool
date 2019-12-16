3c2b1584e2dceb91ac1542e813f1a83d332bc9aa
Mahi2 <mahi2hm@outlook.fr>
2019-12-16 17:17:48 +0100

    [UPDATE] Redirect if the page does not exist

--- controllers/page.php
+++ controllers/page.php
@@ -6,3 +6,9 @@ $custom_page_url = (isset($parameters[0])) ? Database::clean_string($parameters[
 
 /* If the custom page url is set then try to get data from the database */
 $custom_page = ($custom_page_url) ? Database::get('*', 'pages', ['url' => $custom_page_url]) : false;
+
+/* Redirect if the page does not exist */
+if(!$custom_page) {
+	$_SESSION['info'][] = $language->page->info_message->invalid_page;
+	redirect();
+}
