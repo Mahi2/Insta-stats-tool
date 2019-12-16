7bb42ee4407577277c9811c76ccbf8a35df3b8c8
Mahi2 <mahi2hm@outlook.fr>
2019-12-16 18:08:41 +0100

    [UPDATE] Store stripe secret key

---
+++ controllers/stripe.php
@@ -0,0 +1,9 @@
+<?php
+defined('MAHIDCODE') || die();
+
+if(empty($settings->store_stripe_publishable_key) || empty($settings->store_stripe_secret_key) || empty($settings->store_stripe_webhook_secret)) {
+    $_SESSION['info'][] = $language->store->info_message->stripe_not_available;
+    User::get_back('store');
+}
+
+$method = (isset($parameters[0]) && in_array($parameters[0], ['stripe-success', 'stripe-cancel'])) ? $parameters[0] : false;
