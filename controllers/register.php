9d786ad9015ca758d9de33d0b23dbdac98d5cb46
Mahi2 <mahi2hm@outlook.fr>
2019-12-16 17:33:40 +0100

    [UPDATE] Instagram login with register handler

--- controllers/register.php
+++ controllers/register.php
@@ -6,3 +6,18 @@ $redirect = 'dashboard';
 if(isset($_GET['redirect']) && $redirect = $_GET['redirect']) {
     //
 }
+
+/* Default variables */
+$register_username = $register_name = $register_email = '';
+
+/* instagram login / register handler */
+if($settings->instagram_login) {
+
+    $instagram = new MetzWeb\Instagram\Instagram([
+        'apiKey' => $settings->instagram_client_id,
+        'apiSecret' => $settings->instagram_client_secret,
+        'apiCallback' => $settings->url . 'login/instagram'
+    ]);
+
+    $instagram_login_url = $instagram->getLoginUrl();
+}
