edf754757f978f7f3c03f6e9fcfc52139fa1d74d
Mahi2 <mahi2hm@outlook.fr>
2019-12-16 17:19:45 +0100

    [UPDATE] Getting paypal api links

--- controllers/paypal.php
+++ controllers/paypal.php
@@ -1,3 +1,16 @@
 <?php
 defined('MAHIDCODE') || die();
 User::check_permission(0);
+
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Payer;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Amount;
+use PayPal\Api\Transaction;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Payment;
+use PayPal\Api\InputFields;
+use PayPal\Api\WebProfile;
+use PayPal\Api\FlowConfig;
+use PayPal\Api\Presentation;
