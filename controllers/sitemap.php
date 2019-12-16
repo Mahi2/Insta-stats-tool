8c9203028b8f280c6a669bf1439d6c899d5f1c7b
Mahi2 <mahi2hm@outlook.fr>
2019-12-16 17:52:10 +0100

    [UPDATE] Make sure to get the proper total if plugins are active

--- controllers/sitemap.php
+++ controllers/sitemap.php
@@ -8,3 +8,34 @@ header('Content-Type: text/xml');
 
 /* Default pagination */
 $pagination = 9000;
+
+/* Generate the main sitemap */
+if(!is_numeric($limit)) {
+    /* Get total number of ig users in this case */
+    $total = $database->query("SELECT COUNT(*) AS `total` FROM `instagram_users`")->fetch_object()->total;
+
+    /* Make sure to get the proper total if plugins are active */
+    foreach($plugins->plugins as $plugin_identifier => $value) {
+        if($plugins->exists_and_active($plugin_identifier)) {
+
+            $column = $plugin_identifier . '_users';
+
+            $total += $database->query("SELECT COUNT(*) AS `total` FROM `{$column}`")->fetch_object()->total;
+
+        }
+    }
+
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+    echo '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    for ($i = 0; $i < $total; $i += $pagination) {
+
+        echo '
+        <sitemap>
+            <loc>' . $settings->url . 'sitemap/' . $i . '</loc>
+            <lastmod>' . (new DateTime())->format('Y-m-d\TH:i:sP') . '</lastmod>
+        </sitemap>';
+
+    }
+
+    echo '</sitemapindex>';
+}
