f202a18190111210d4ae6d33ff0ee11487486c32
Mahi2 <mahi2hm@outlook.fr>
2019-12-16 17:34:13 +0100

    [UPDATE] Facebook login register

--- controllers/register.php
+++ controllers/register.php
@@ -21,3 +21,16 @@ if($settings->instagram_login) {
 
     $instagram_login_url = $instagram->getLoginUrl();
 }
+
+/* Facebook Login / Register */
+if($settings->facebook_login) {
+
+    $facebook = new Facebook\Facebook([
+        'app_id' => $settings->facebook_app_id,
+        'app_secret' => $settings->facebook_app_secret,
+        'default_graph_version' => 'v2.2',
+    ]);
+
+    $facebook_helper = $facebook->getRedirectLoginHelper();
+    $facebook_login_url = $facebook->getRedirectLoginHelper()->getLoginUrl($settings->url . 'login/facebook', ['email', 'public_profile']);
+}
