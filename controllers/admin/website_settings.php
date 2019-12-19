95a971c7ff15a11b31807330c00fae79642ea689
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:52:25 +0100

    [UPDATE] Delete the current log

--- controllers/admin/website_settings.php
+++ controllers/admin/website_settings.php
@@ -7,3 +7,7 @@ $url_token 	= (isset($parameters[1])) ? $parameters[1] : false;
 
 
 if($method && $method == 'remove-logo' && $url_token && Security::csrf_check_session_token('url_token', $url_token)) {
+    /* Delete the current log */
+    if(!empty($settings->logo) && file_exists(ROOT . UPLOADS_ROUTE . 'logo/' . $settings->logo)) {
+        unlink(ROOT . UPLOADS_ROUTE . 'logo/' . $settings->logo);
+    }
