59fcfffc694ff7e4ddaa25e6ca1c420afaf57beb
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:37:52 +0100

    [UPDATE] Check for the required fields

--- controllers/admin/proxy_management.php
+++ controllers/admin/proxy_management.php
@@ -74,3 +74,33 @@ if(isset($type) && $type == 'test') {
 }
 
 if(!empty($_POST)) {
+
+    /* Filter some the variables */
+	$_POST['address'] = Database::clean_string($_POST['address']);
+    $_POST['port'] = (int) Database::clean_string($_POST['port']);
+    $_POST['username'] = trim(Database::clean_string($_POST['username']));
+    $_POST['password'] = trim(Database::clean_string($_POST['password']));
+    $_POST['note'] = Database::clean_string($_POST['note']);
+    $_POST['method'] = (int) Database::clean_string($_POST['method']);
+
+    $default_values = [
+        'address' => $_POST['address'],
+        'port' => $_POST['port'],
+        'username' => $_POST['username'],
+        'password' => $_POST['password'],
+        'note' => $_POST['note']
+    ];
+
+	$required_fields = ['address', 'port'];
+
+	/* Check for the required fields */
+	foreach($_POST as $key => $value) {
+		if(empty($value) && in_array($key, $required_fields)) {
+			$_SESSION['error'][] = $language->global->error_message->empty_fields;
+			break 1;
+		}
+	}
+
+    if(!Security::csrf_check_session_token('form_token', $_POST['form_token'])) {
+        $_SESSION['error'][] = $language->global->error_message->invalid_token;
+    }
