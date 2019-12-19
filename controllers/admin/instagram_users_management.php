e7d80d76f0f56cd57f2ea55930bc0e780acc1fac
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:18:41 +0100

    [EDIT] Insert the needed librairies

--- controllers/admin/instagram_users_management.php
+++ controllers/admin/instagram_users_management.php
@@ -25,3 +25,12 @@ if(isset($type) && $type == 'delete') {
 
     redirect('admin/instagram-users-management');
 }
+
+/* Insert the needed libraries */
+add_event('head', function() {
+    global $settings;
+
+    echo '<link href="' . $settings->url . ASSETS_ROUTE . 'css/datatables.min.css" rel="stylesheet" media="screen">';
+    echo '<script src="' . $settings->url . ASSETS_ROUTE . 'js/datatables.min.js"></script>';
+
+});
