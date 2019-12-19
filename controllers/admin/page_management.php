04116930c7c58ec6732bef263081384a19fafc31
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:30:35 +0100

    [EDIT] Checking for errors

--- controllers/admin/page_management.php
+++ controllers/admin/page_management.php
@@ -7,3 +7,16 @@ $page_id 	= (isset($parameters[1])) ? (int) $parameters[1] : false;
 $url_token	= (isset($parameters[2])) ? $parameters[2] : false;
 
 if(isset($type) && $type == 'delete') {
+    /* Check for errors and permissions */
+	if(!Security::csrf_check_session_token('url_token', $url_token)) {
+		$_SESSION['error'][] = $language->global->error_message->invalid_token;
+	}
+
+
+	if(empty($_SESSION['error'])) {
+		$database->query("DELETE FROM `pages` WHERE `page_id` = {$page_id}");
+
+		$_SESSION['success'][] = $language->global->success_message->basic;
+	}
+
+}
