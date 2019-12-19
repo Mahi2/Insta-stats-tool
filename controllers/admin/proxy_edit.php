4febafd4b8167fc298b12849c53d9be0f7e06c31
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:40:35 +0100

    [UPDATE] Filter some the variables

--- controllers/admin/proxy_edit.php
+++ controllers/admin/proxy_edit.php
@@ -9,3 +9,14 @@ if(!$proxy = Database::get('*', 'proxies', ['proxy_id' => $proxy_id])) {
     $_SESSION['error'][] = $language->admin_proxy_edit->error_message->invalid_page;
     User::get_back('admin/proxies-management');
 }
+
+if(!empty($_POST)) {
+    /* Filter some the variables */
+    $_POST['address'] = Database::clean_string($_POST['address']);
+    $_POST['port'] = (int) Database::clean_string($_POST['port']);
+    $_POST['username'] = trim(Database::clean_string($_POST['username']));
+    $_POST['password'] = trim(Database::clean_string($_POST['password']));
+    $_POST['note'] = Database::clean_string($_POST['note']);
+    $_POST['method'] = (int) Database::clean_string($_POST['method']);
+
+    $required_fields = ['address', 'port'];
