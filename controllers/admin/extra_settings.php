cc161291623eb7dedfb876ffaa06fee479b8acc8
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:07:06 +0100

    [UPDATE] Check for errors and permissions

--- controllers/admin/extra_settings.php
+++ controllers/admin/extra_settings.php
@@ -8,3 +8,23 @@ $url_token	= isset($parameters[2]) ? $parameters[2] : false;
 $source     = isset($parameters[3]) && in_array($parameters[3], $sources) ? $parameters[3] : reset($sources);
 
 if(isset($type) && $type == 'plugin_status') {
+    /* Check for errors and permissions */
+    if(!Security::csrf_check_session_token('url_token', $url_token)) {
+        $_SESSION['error'][] = $language->global->error_message->invalid_token;
+    }
+
+    if(!$plugin = Database::get(['status'], 'plugins', ['identifier' => $id])) {
+        redirect('admin/extra-settings');
+    }
+
+    if(empty($_SESSION['error'])) {
+        $new_status = (int) !$plugin->status;
+        $database->query("UPDATE `plugins` SET `status` = {$new_status} WHERE `identifier` = '{$id}'");
+
+        $_SESSION['success'][] = $language->global->success_message->basic;
+
+        redirect('admin/extra-settings');
+    }
+}
+
+if(isset($type) && $type == 'demo_delete') {
