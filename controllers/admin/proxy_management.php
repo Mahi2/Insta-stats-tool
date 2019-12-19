7240ca146d9a0daa48579e55a0bec65c0bed0cd5
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:35:50 +0100

    [EDIT] Check for permissions

--- controllers/admin/proxy_management.php
+++ controllers/admin/proxy_management.php
@@ -15,3 +15,21 @@ $default_values = [
 ];
 
 if(isset($type) && $type == 'delete') {
+
+    /* Check for errors and permissions */
+	if(!Security::csrf_check_session_token('url_token', $url_token)) {
+		$_SESSION['error'][] = $language->global->error_message->invalid_token;
+	}
+
+	if(empty($_SESSION['error'])) {
+		$database->query("DELETE FROM `proxies` WHERE `proxy_id` = {$proxy_id}");
+
+		$_SESSION['success'][] = $language->global->success_message->basic;
+	}
+
+    redirect('admin/proxies-management');
+}
+
+if(isset($type) && $type == 'test') {
+
+    $proxy = Database::get('*', 'proxies', ['proxy_id' => $proxy_id]);
