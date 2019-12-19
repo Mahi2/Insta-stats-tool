8205266827984f481653b7826ae2cebc89172d56
Mahi2 <mahi2hm@outlook.fr>
2019-12-19 13:14:25 +0100

    [UPDATE] Insert the chartjs library

--- controllers/admin/index.php
+++ controllers/admin/index.php
@@ -52,3 +52,10 @@ while($data = $payments_month_result->fetch_object()) {
 
 /* Defining the chart data */
 $payments_month_chart = generate_chart_data($payments_month_chart);
+
+/* Insert the chartjs library */
+add_event('head', function() {
+    global $settings;
+
+    echo '<script src="' . $settings->url . ASSETS_ROUTE . 'js/Chart.bundle.min.js"></script>';
+});
