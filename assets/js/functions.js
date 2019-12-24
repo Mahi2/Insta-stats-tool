90838e7a131578f1e82667607e1e221a0685f2ed
Mahi2 <mahi2hm@outlook.fr>
2019-12-24 17:53:04 +0100

    [UPDATE] Success working properly

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,5 +4,23 @@ let favorite = (event) => {
     let source = $event.data('source');
     $event.fadeOut();
 
+    $.ajax({
+        type: 'POST',
+        url: 'favorites',
+        data: {
+            source_user_id,
+            source
+        },
+        success: (data) => {
+            if(data.status == 'error') {
+                alert('Please try again later, something is not working properly.');
+            }
+
+            else if(data.status == 'success') {
+                $event.fadeIn().html(data.details.html);
+            }
+        },
+        dataType: 'json'
+    });
 
 }
